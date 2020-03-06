4eeaf5b3f15ccf7b7ceccd0127e4b4a31544c461
prathibha <garipellyprathibha@gmail.com>
2020-03-06 22:45:28 +0530

    epam_task1

---
+++ First.java
@@ -0,0 +1,13 @@
+package first;
+import java.io.*;
+import java.util.*;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
